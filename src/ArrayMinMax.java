85d1894cf675cf8ab59f204fc057fca1133c0e4f
Dawood Magsi <dawoodmagsi0@gmail.com>
2024-07-23 14:03:32 +0500

    main

---
+++ src/ArrayMinMax.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class ArrayMinMax {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+
+        int[] array = new int[size];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        int max = findMax(array);
+        int min = findMin(array);
+
+        System.out.println("Maximum element: " + max);
+        System.out.println("Minimum element: " + min);
+
+        scanner.close();
+    }
+
+    public static int findMax(int[] array) {
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+
+    // Method to find the minimum element in the array
+    public static int findMin(int[] array) {
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+}
